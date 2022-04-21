5c438aefdc08971a50e92d0312f5d52004f55be7
Lautaro Vera <lautarovera93@gmail.com>
2022-04-21 19:04:15 +0200

    Removing from Debug folder files which have to be ignored

--- PCSE_LIS2DW12_Driver/Debug/Drivers/LIS2DW12/src/lis2dw12.d
+++
@@ -1,7 +0,0 @@
-Drivers/LIS2DW12/src/lis2dw12.o: ../Drivers/LIS2DW12/src/lis2dw12.c \
- /home/lautaro/CESE/2_Bimestre/PCSE/cese-pcse-driver/PCSE_LIS2DW12_Driver/Drivers/LIS2DW12/inc/lis2dw12.h \
- /home/lautaro/CESE/2_Bimestre/PCSE/cese-pcse-driver/PCSE_LIS2DW12_Driver/Drivers/LIS2DW12/inc/lis2dw12_cfg.h \
- /home/lautaro/CESE/2_Bimestre/PCSE/cese-pcse-driver/PCSE_LIS2DW12_Driver/Drivers/LIS2DW12/inc/lis2dw12_stm32f429xx_port.h
-/home/lautaro/CESE/2_Bimestre/PCSE/cese-pcse-driver/PCSE_LIS2DW12_Driver/Drivers/LIS2DW12/inc/lis2dw12.h:
-/home/lautaro/CESE/2_Bimestre/PCSE/cese-pcse-driver/PCSE_LIS2DW12_Driver/Drivers/LIS2DW12/inc/lis2dw12_cfg.h:
-/home/lautaro/CESE/2_Bimestre/PCSE/cese-pcse-driver/PCSE_LIS2DW12_Driver/Drivers/LIS2DW12/inc/lis2dw12_stm32f429xx_port.h:
